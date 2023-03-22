8e1ce71000f58dd57e8e0855f2ac22388b503f2e
Torben Schweren <torben@schweren.dev>
2023-03-22 20:37:08 +0100

    NavigationBar component
    NavigationBar is now its own component.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,11 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <NavigationBar />
   <router-view />
 </template>
+
+<script setup lang="ts">
+import NavigationBar from "./components/NavigationBar.vue";
+</script>
 
 <style lang="scss">
 #app {
@@ -13,18 +14,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
