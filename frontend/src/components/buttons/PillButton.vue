6f2f9aa49d0efc170b7d52354bc6eeb4797420b8
Torben Schweren <torben@schweren.dev>
2023-04-02 21:47:55 +0200

    PillButton component
    Added PillButton component

---
+++ frontend/src/components/buttons/PillButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <button class="pill">
+    <slot />
+  </button>
+</template>
+
+<style scoped lang="scss">
+//Make button pill shaped
+.pill {
+  display: inline-block;
+  background: linear-gradient(to left, #525252 50%, #bc956a 50%);
+  background-size: 200% 100%;
+  background-position: right bottom;
+  color: #ffffff;
+  border: none;
+  border-radius: 16px;
+  padding: 0.5rem 2rem;
+  text-align: center;
+  text-decoration: none;
+  cursor: pointer;
+  transition: all 0.33s ease-out;
+}
+
+.pill:hover {
+  background-position: left bottom;
+}
+</style>
